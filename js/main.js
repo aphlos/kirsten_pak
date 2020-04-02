5cb26a4a72b98a6384241f5f0ebf85b6c73cc535
Chloe Kim <chloeminkyung@gmail.com>
2020-04-02 21:27:34 +0800

    update

--- js/main.js
+++ js/main.js
@@ -336,19 +336,6 @@
 
   function initVideoBg(){
 
-    if ($('.player').length) {
-      $('.player').mb_YTPlayer({
-        containment: '#video-wrapper',
-        autoPlay: true,
-        mute: true
-      });
-
-      if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('#video-wrapper').append('<div id="fallback-bg"></div>');
-        $('#fallback-bg').css('background-image', 'url('+$('#video-wrapper').data('fallback-bg')+')');
-      }
-    }
-
     var videoEl = $('#video-wrapper video');
 
     var setProportion = function () {
